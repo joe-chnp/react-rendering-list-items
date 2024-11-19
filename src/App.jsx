52fdf0ab624460a1a091e5fc01c72a5fd3a1b8d4
Joe <chanaphat.sip@gmail.com>
2024-11-19 21:31:15 +0700

    fe-3-joe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
-      <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
-      </section>
+      {movies.map((movie, index) => {
+        return (
+          <section key={index} className="movie-list-section">
+            <img src={movie.image}/>
+            <div className="container">
+              <div>Title: {movie.title}</div>
+              <div>Year: {movie.year}</div>
+              <div>Runtime: {movie.runtime}</div>
+              <div className="genres">Genres: {
+                <div className="genreContainer">
+                  {
+                    movie.genres.map((genre, index) => {
+                      return (
+                        <span className="genre" key={index}>{genre}</span>
+                      )
+                    })
+                  }
+                </div>
+                
+              }</div>
+              <div>IMDB Ratings: {movie.imdbRating}</div>
+              <div>IMDB Votes: {movie.imdbVotes}</div>
+            </div>
+          </section>
+        )
+      })}
     </div>
   );
 }
